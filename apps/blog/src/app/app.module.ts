a5f4571edf7abb297d24b29a1366cebbf4ebd444
tomavic <hbasheer@live.com>
2022-05-22 18:35:33 +0200

    feat: add bootstrap

--- apps/blog/src/app/app.module.ts
+++ apps/blog/src/app/app.module.ts
@@ -4,10 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatProgressBarModule, MatCardModule, MatButtonModule, MatDividerModule],
   providers: [],
   bootstrap: [AppComponent],
 })
